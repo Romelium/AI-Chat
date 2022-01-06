451a68cac535cdb03dd0e3c816007b90eedce862
Romelianism <Romelianism@gmail.com>
2022-01-06 22:33:04 +0800

    Add demo of random words ---------------- Add messager Add api fetch Add secret

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,72 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React, { useState } from 'react'
+
+function Message({ children, right }: { children: string, right: boolean }) {
+  return <div className={styles.message + ' ' + (right === true ? styles.right : '')}>{children}</div>
+}
+
+function Messages({ right, messages }: { right: boolean, messages: string[] }) {
+  let messagesElement = Array<JSX.Element>()
+  for (let i in messages) {
+    messagesElement.push(<Message right={right = !right} key={i}>{messages[i]}</Message>);
+  }
+  return (
+    <div className={styles.messages}>
+      {messagesElement}
+    </div>
+  )
+}
+function Sender({ onSubmit }: { onSubmit: (message: string) => void; }) {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    onSubmit(message)
+    console.log("Sent: " + message)
+  }
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <textarea id="message" name="message" value={message} onChange={e => setMessage(e.target.value)} />
+      <button type="submit">Send</button>
+    </form>
+  )
+}
+
+class Messager extends React.Component<{}, { messages: string[] }> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      messages: Array<string>(),
+    }
+  }
+
+  async AddMessage(message: string) {
+    let messages = this.state.messages.slice();
+    messages.push(message); //add string to the messages
+
+    //basic demo of random words
+    const data = await fetch('https://random-word-api.herokuapp.com/word?number=10')
+      .then(res => res.json() as Promise<Array<string>>)
+    console.log(data)
+    messages.push(data.join(' '))
+
+    this.setState({
+      messages: messages
+    })
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div>
+        <Messages right={false} messages={this.state.messages}></Messages>
+        <Sender onSubmit={this.AddMessage.bind(this)}></Sender>
+      </div>
+    )
+  }
+}
 
 const Home: NextPage = () => {
   return (
@@ -18,11 +84,13 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-        Chat with an AI-powered by{' '} <a href="https://github.com/kingoflolz/mesh-transformer-jax.git">GPT-j</a>. Talk with it, set parameters, and ask questions.
+          Chat with an AI-powered by{' '} <a href="https://github.com/kingoflolz/mesh-transformer-jax.git">GPT-j</a>. Talk with it, set parameters, and ask questions.
         </p>
 
+        <Messager></Messager>
+
         <div className={styles.grid}>
-          <a href="https://github.com/Romelianism/AI-Chat.git" className={styles.card}>
+          <a href="https://github.com/Romelianism/AI-Chat.git" target="_blank" className={styles.card} rel="noreferrer">
             <h2>Source Code &rarr;</h2>
             <p>Give feed back and ideas of how to make this better.</p>
           </a>
