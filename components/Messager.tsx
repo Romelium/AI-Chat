d5c91456eb03cda2eadbb57cda6824d18fd45781
Romelianism <Romelianism@gmail.com>
2022-01-15 16:18:31 +0800

    Format Messager

--- components/Messager.tsx
+++ components/Messager.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import { Sender } from "./Sender";
 import { Messages } from "./Messages";
 import { Responder, EchoResponder } from "./Responder";
-import ContentEditable from 'react-contenteditable'
+import ContentEditable from "react-contenteditable";
 
 interface MessagerProps {
   responder: Responder;
@@ -10,7 +10,10 @@ interface MessagerProps {
   contenteditable: boolean;
   onAddMessage: (messages: string[]) => void;
 }
-export class Messager extends React.Component<MessagerProps, { messages: string[]; waiting: Boolean; }> {
+export class Messager extends React.Component<
+  MessagerProps,
+  { messages: string[]; waiting: Boolean }
+> {
   constructor(props: MessagerProps) {
     super(props);
     this.state = {
@@ -22,41 +25,45 @@ export class Messager extends React.Component<MessagerProps, { messages: string[
     responder: new EchoResponder(),
     text_to_speech: false,
     contenteditable: false,
-    onAddMessage: () => { }
+    onAddMessage: () => {},
   };
 
   async addMessage(message: string) {
-    if(this.state.waiting == true){
-    let messages = this.state.messages.slice();
-    messages.push(message);
-    this.setState({ messages: messages });
-    try {
-      const res = await this.props.responder.Response(messages);
-      messages.push(res); //add string to the messages
+    if (!this.state.waiting) {
+      let messages = this.state.messages.slice();
+      messages.push(message);
+      this.setState({ messages: messages, waiting: true});
+      try {
+        const res = await this.props.responder.Response(messages);
+        messages.push(res); //add string to the messages
 
-      this.props.onAddMessage(messages);
-      this.setState({ messages: messages });
+        this.props.onAddMessage(messages);
+        this.setState({ messages: messages, waiting: false});
 
-      if (this.props.text_to_speech) {
-        const msg = new SpeechSynthesisUtterance(res);
-        window.speechSynthesis.speak(msg);
+        if (this.props.text_to_speech) {
+          const msg = new SpeechSynthesisUtterance(res);
+          window.speechSynthesis.speak(msg);
+        }
+      } catch {
+        messages.pop();
+        this.setState({ messages: messages, waiting: false});
       }
-    }
-    catch {
-      messages.pop();
-      this.setState({ messages: messages });
-    }
     }
   }
 
   render(): React.ReactNode {
     return (
       <>
-        <Messages contenteditable={this.props.contenteditable} right={false} messages={this.state.messages} onChange={((i: number, e: React.FormEvent<HTMLDivElement>) => {
-          const messages = this.state.messages.slice();
-          messages[i] = e.currentTarget.innerText;
-          this.setState({ messages: messages });
-        }).bind(this)}></Messages>
+        <Messages
+          contenteditable={this.props.contenteditable}
+          right={false}
+          messages={this.state.messages}
+          onChange={((i: number, e: React.FormEvent<HTMLDivElement>) => {
+            const messages = this.state.messages.slice();
+            messages[i] = e.currentTarget.innerText;
+            this.setState({ messages: messages });
+          }).bind(this)}
+        ></Messages>
         <Sender onSubmit={this.addMessage.bind(this)}></Sender>
       </>
     );
