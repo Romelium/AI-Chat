e2b750dcd8712a76e5bff7293e298ef4d0385f56
Romelianism <Romelianism@gmail.com>
2022-01-16 14:20:59 +0800

    Add NameDescription for you

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -10,7 +10,7 @@ export function Chat() {
   const [your_name, setYour_name] = useState("you");
   const [AI_name, setAI_name] = useState("gpt-j");
   const [pre_context, setPre_context] = useState(
-    "This is a chat between a ${your_name} and ${ai_name}. ${ai_name} is very nice and funny."
+    "This is a chat between a ${your_name} and ${AI_name}.\n${your_name} are a person.\n${AI_name} is very nice and funny."
   );
   const [temperature, setTemperature] = useState(1);
   const [top_p, setTop_p] = useState(1);
@@ -41,18 +41,18 @@ export function Chat() {
         setPre_context={setPre_context}
         nameDescriptions={
           [
-            {name: 'you', description: 'This is a chat between a ${your_name} and ${ai_name}. ${ai_name} are very nice and funny.'},
-            {name: 'he', description: 'This is a chat between a ${your_name} and him. ${ai_name} is a good person and hard worker.'},
-            {name: 'she', description: 'This is a chat between a ${your_name} and her. ${ai_name} is a good person and hard worker.'},
-            {name: 'god', description: 'This is a chat between a ${your_name} and ${ai_name}. ${ai_name} is supreme being, creator, and principal object of faith.'},
-            {name: 'gpt-j', description: pre_context},
-            {name: 'obama', description: 'This is a chat between a ${your_name} and ${ai_name}. ${ai_name} is an American politician, lawyer, and author who served as the 44th president of the United States.'},
-            {name: 'trump', description: 'This is a chat between a ${your_name} and ${ai_name}. ${ai_name} is an American politician, media personality, and businessman who served as the 45th president of the United States.'},
-            {name: 'the devil', description: 'This is a chat between a ${your_name} and ${ai_name}. ${ai_name} is the personification of evil as it is conceived in various cultures and religious traditions.'},
-            {name: 'the president', description: 'This is a chat between a ${your_name} and ${ai_name}. ${ai_name} is a very prominent figure in the country and nice to the people that support it.'},
-            {name: 'the scientist', description: 'This is a chat between a ${your_name} and ${ai_name}. ${ai_name} is a person who conducts scientific research to advance knowledge in an area of interest.'},
-            {name: 'the physicist', description: 'This is a chat between a ${your_name} and ${ai_name}. ${ai_name} is a scientist who specializes in the field of physics, which encompasses the interactions of matter and energy at all length and time scales in the physical universe.'},
-            {name: 'the mathematician', description: 'This is a chat between a ${your_name} and ${ai_name}. ${ai_name} is someone who uses an extensive knowledge of mathematics in their work, typically to solve mathematical problems.'},
+            {name: 'you', description: 'are a person'},
+            {name: 'he', description: 'is a good person and hard worker'},
+            {name: 'she', description: 'is a good person and hard worker'},
+            {name: 'god', description: 'is supreme being, creator, and principal object of faith'},
+            {name: 'gpt-j', description: 'is very nice and funny'},
+            {name: 'obama', description: 'is an American politician, lawyer, and author who served as the 44th president of the United States'},
+            {name: 'trump', description: 'is an American politician, media personality, and businessman who served as the 45th president of the United States'},
+            {name: 'the devil', description: 'is the personification of evil as it is conceived in various cultures and religious traditions'},
+            {name: 'the president', description: 'is a very prominent figure in the country and nice to the people that support it'},
+            {name: 'the scientist', description: 'is a person who conducts scientific research to advance knowledge in an area of interest'},
+            {name: 'the physicist', description: 'is a scientist who specializes in the field of physics, which encompasses the interactions of matter and energy at all length and time scales in the physical universe'},
+            {name: 'the mathematician', description: 'This is a chat between a ${your_name} and ${ai_name}. ${ai_name} is someone who uses an extensive knowledge of mathematics in their work, typically to solve mathematical problems'},
           ]
         }
       />
